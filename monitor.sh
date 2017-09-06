25059222680481e911c9e8cb27cb7f822869f0c3
Paininabox <paininabox@gmail.com>
2017-09-06 00:47:09 -0600

    fixed bar on monitor script

--- monitor.sh
+++ monitor.sh
@@ -6,8 +6,12 @@ monitor=DVI-I-1
 if [ -z "$(xrandr --verbose | grep 'DVI-I-1 connected 1440')" ];then
 
 	xrandr --output $monitor --auto && xrandr --output HDMI-0 --left-of $monitor && xset +dpms
+        sleep 3
+        i3-msg restart
 else
 
 	xrandr --output $monitor --off && xset -dpms && xset s 0
+        sleep 3
+        i3-msg restart
 
 fi
