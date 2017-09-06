b5573c75a11f8b8fbbeb669f1885b9393fb0e2a9
Paininabox <paininabox@gmail.com>
2017-09-06 00:29:38 -0600

    made one monitor mode turn off screen sleeping

--- monitor.sh
+++ monitor.sh
@@ -8,6 +8,6 @@ if [ -z "$(xrandr --verbose | grep 'DVI-I-1 connected 1440')" ];then
 	xrandr --output $monitor --auto && xrandr --output HDMI-0 --left-of $monitor && xset +dpms
 else
 
-	xrandr --output $monitor --off && xset -dpms
+	xrandr --output $monitor --off && xset -dpms && xset s 0
 
 fi
