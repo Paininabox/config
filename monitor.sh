27a3092578dcbda8a6f4e4832fc9c7b3db42225c
Paininabox <paininabox@gmail.com>
2017-10-06 15:02:21 -0600

    fixed monitor mirroring after shutoff

--- monitor.sh
+++ monitor.sh
@@ -5,7 +5,7 @@ monitor=GPU-1.HDMI-0
 
 if [ -z "$(xrandr --verbose | grep 'GPU-1.HDMI-0 connected 1920')" ];then
 
-	xrandr --output $monitor --auto 
+	xrandr --output $monitor --auto --right-of GPU-0.HDMI-0
         sleep 3
         i3-msg restart
 else
