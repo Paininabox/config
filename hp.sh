3843333d7cfebb49ab6406a585a00a4af35047dd
Paininabox <paininabox@gmail.com>
2018-04-26 22:48:52 -0600

    getting desktop lights to work

--- hp.sh
+++ hp.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # My first script
-hp=alsa_output.pci-0000_07_00.0.analog-surround-51
+#hp=alsa_output.pci-0000_07_00.0.analog-surround-51
+hp=alsa_output.pci-0000_07_00.0.analog-stereo
 #echo "Setting default sink to: $1";
 pacmd set-default-sink $hp
 pacmd list-sink-inputs | grep index | while read line
